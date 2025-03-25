c096d8d7ee50da7ac884b0cb4c957ea313e38237
Gamer100Dev <115351738+Gamer100Dev@users.noreply.github.com>
2025-03-25 12:34:18 +0200

    Update gamer_compositor.c

--- src/gamer_compositor.c
+++ src/gamer_compositor.c
@@ -12,6 +12,7 @@
 #include <wlr/types/wlr_cursor.h>
 #include <wlr/types/wlr_compositor.h>
 #include <wlr/types/wlr_data_device.h>
+#include <wlr/types/wlr_server_decoration.h>
 #include <wlr/types/wlr_input_device.h>
 #include <wlr/types/wlr_keyboard.h>
 #include <wlr/types/wlr_output.h>
